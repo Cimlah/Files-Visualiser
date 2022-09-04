1ee076e1e7c7c2d4ee00a73beaa4c8cca0ba0f6c
Michał Stolarczyk <28655852+Cimlah@users.noreply.github.com>
2022-09-04 22:07:48 +0200

    List of files is displayed on client side inside HTML file

--- content/js/client-index.js
+++ content/js/client-index.js
@@ -1,12 +1,27 @@
 const testButton = document.querySelector('.test-button')
+const fileList = document.querySelector('.file-list')
 
 const host = 'localhost:3000'
 const url = 'http://' + host + '/data'
+let files = []
 
-const sendGet = () => {
-    axios.get(url).then((response) => {
-        console.log(response.data)
-    })
+async function sendGet() {
+    try {
+        const response = await axios.get(url);
+        files = response.data
+
+        function displayFileList() {
+            for(let i = 0; i < files.length; i++) {
+                const file = document.createElement('div')
+                file.classList.add('file')
+                const fileName = document.createTextNode(files[i])
+                file.appendChild(fileName)
+                fileList.appendChild(file)
+            }
+        } displayFileList()
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 testButton.addEventListener("click", sendGet)
