e81a5de23eff31fb27ec9a2e162e0aa23ce8d16f
Michał Stolarczyk <28655852+Cimlah@users.noreply.github.com>
2022-09-04 12:12:11 +0200

    Created serving an HTML file over HTTP

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const express = require('express');
+const express = require('express')
+const path = require('path')
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
 app.get('/', function(req, res) {
-    res.send('Hello Express!');
-});
+    res.sendFile(path.join(__dirname, 'html/index.html'))
+})
 
 app.listen(port, (err) => {
     if(err) {
-        console.log('Something went wrong\n' + err);
+        console.log('Something went wrong\n' + err)
     }
 
     else {
-        console.log('Server is running on port ' + port);
+        console.log('Server is running on port ' + port)
     }
-});
+})
